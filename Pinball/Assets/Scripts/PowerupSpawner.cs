04a0436248485c123960cc87b3cd3cd0e6d2f610
Justin Bishay <justinb@Justins-MacBook-Pro-2.local>
2017-09-15 21:11:44 -1000

    updated powerup script and spawning method

---
+++ Pinball/Assets/Scripts/PowerupSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour {
+
+	public GameObject[] powerups;
+	public GameObject ball;
+	private ScoreKeeper ScoringObject;
+	public Vector3 spawnValues;
+	private int limit = 10;
+	private GameObject token;
+
+	// Use this for initialization
+	void Start () {
+		ScoringObject = GameObject.Find ("Camera").GetComponent<ScoreKeeper> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		token = powerups [Random.Range (0, powerups.Length)];
+		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+
+		if (ScoringObject.TotalScore > limit) {
+			Instantiate (token, spawnPosition, spawnRotation);
+			limit += 100;
+		}
+	}
+		
+}
+
--- Pinball/Assets/Scripts/Powerups.cs
+++ Pinball/Assets/Scripts/Powerups.cs
@@ -6,24 +6,60 @@ public class Powerups : MonoBehaviour {
 
 
 	public GameObject token;
-	public GameObject ball; 
-
+	private bool left = true;
+	private bool right = false;
+	public float moveSpeed;
+	private float rotateSpeed;
+	private PowerupSpawner spawner;
 
 	// Use this for initialization
 	void Start () {
-		
+		spawner = GameObject.Find ("Powerups").GetComponent<PowerupSpawner> ();
+		rotateSpeed = 50;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		MoveHorizontal ();
+		Rotate ();
 	}
 
 	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.name == ball.name) {
-			Destroy (token);
+		Destroy (token);
+	}
+
+
+	void MoveHorizontal (){
+		
+		Transform body = token.GetComponent<Transform> ();
+		float move = moveSpeed * Time.deltaTime;
+
+		if (left) {
+			body.Translate (-move, 0, 0, Space.World); 
+		}
+
+		else if (right) {
+			body.Translate (move, 0, 0, Space.World); 
+		}
+
+		if (body.position.x <= -spawner.spawnValues.x) {
+			right = true;
+			left = false;
+		}
+
+		if (body.position.x >= spawner.spawnValues.x) {
+			left = true;
+			right = false;
 		}
 	}
+
+	void Rotate () {
+		Transform body = token.GetComponent<Transform> ();
+		float move = rotateSpeed * Time.deltaTime;
+
+		body.Rotate (0, move, 0, Space.World);
+	}
+
 
 }
 
